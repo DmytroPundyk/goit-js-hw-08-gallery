5003ce92691b01bcd4e1a618ab8da26f12b6bfbc
DmytroPundyk <dimonpund@gmail.com>
2021-09-20 22:20:12 +0300

    ДЗ№8

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-import gallery from "./app.js";
+import galleryItems from "./app.js";
 
 const refs = {
    galleryContainerEl: document.querySelector(".js-gallery"),
@@ -8,8 +8,8 @@ const refs = {
    closeLightBoxBtnEl: document.querySelector(`[data-action = "close-lightbox"]`),
    
 };
-const imagesListMarkup = gallery
-   .map(({ preview, original, description }) =>
+const imagesListMarkup = galleryItems
+   .map(({ preview, original, description },) =>
       `<li class="gallery__item">
   <a class="gallery__link"
     href="${original}" >
@@ -23,6 +23,30 @@ const imagesListMarkup = gallery
 </li>`
 ).join("");
    
-refs.galleryContainerEl.insertAdjacentHTML("afterend", imagesListMarkup);
+refs.galleryContainerEl.insertAdjacentHTML("afterbegin", imagesListMarkup);
 
+function openOverlayClick(evt) {
+  evt.preventDefault();
+  const originalImg = evt.target.dataset.source;
+  const currentAlt = evt.target.alt;
+
+  if (evt.target.nodeName === "IMG") {
+    refs.lightBoxContainerEl.classList.add("is-open");
+    refs.imageLightBoxEl.setAttribute("src", `${originalImg}`);
+    refs.imageLightBoxEl.setAttribute("alt", `${currentAlt}`);
+  }
+};
+refs.galleryContainerEl.addEventListener('click', openOverlayClick);
+
+function closeOverlayClick(evt) {
+  if (evt.target.nodeName === "BUTTON" &&
+    evt.target.dataset.action === "close-lightbox") {
+    refs.lightBoxContainerEl.classList.remove("is-open");
+  }
+  if (evt.target.nodeName === "DIV") {
+     refs.lightBoxContainerEl.classList.remove("is-open");
+  }
+};
+refs.closeLightBoxBtnEl.addEventListener("click", closeOverlayClick);
+refs.overlayBox.addEventListener("click", closeOverlayClick);
    
